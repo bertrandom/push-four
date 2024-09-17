e112d57197e502c040a66f77dfc10567ee8c988e
Bertrand Fan <bert@bert.org>
2024-09-17 11:12:17 -0700

    Use allowlist

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 # shellcheck disable=SC2046
 [ -f .env ] && export $(grep -v '^#' .env | xargs)
 
-WORD=$(shuf -n 1 four_letter_words.txt | tr -d '\n')
+WORD=$(shuf -n 1 aurora_words.txt | tr -d '\n')
 
 mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USERNAME -P $MQTT_PASSWORD -t "iot/flipdisc" -m $WORD
